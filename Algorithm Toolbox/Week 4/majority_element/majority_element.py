269cd978625b4c608c486fa12e61d781e7a00479
supermikol <mdu123@gmail.com>
2016-05-13 22:59:11 -0700

    determine majority problem

--- Algorithm Toolbox/Week 4/majority_element/majority_element.py
+++ Algorithm Toolbox/Week 4/majority_element/majority_element.py
@@ -7,6 +7,19 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
+    mid = left + (right - left) // 2
+    left_major = get_majority_element(a, left, mid)
+    right_major = get_majority_element(a, mid, right)
+    left_count, right_count = 0, 0
+    for i in range(left,right):
+        if a[i] == left_major:
+            left_count += 1
+        if a[i] == right_major:
+            right_count += 1
+    if left_count > (right - left) // 2:
+        return left_major
+    elif right_count > (right - left) // 2:
+        return right_major
     return -1
 
 if __name__ == '__main__':
