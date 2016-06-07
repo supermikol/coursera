d827e495cbc39a2c7bf48245cc4801e25c18f2f7
supermikol <mdu123@gmail.com>
2016-06-07 02:58:50 -0700

    complete week 3 challenges

--- Machine Learning/Week 3/ex2/mapFeature.m
+++ Machine Learning/Week 3/ex2/mapFeature.m
@@ -4,7 +4,7 @@ function out = mapFeature(X1, X2)
 %   MAPFEATURE(X1, X2) maps the two input features
 %   to quadratic features used in the regularization exercise.
 %
-%   Returns a new feature array with more features, comprising of 
+%   Returns a new feature array with more features, comprising of
 %   X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
 %
 %   Inputs X1, X2 must be the same size
@@ -17,5 +17,4 @@ for i = 1:degree
         out(:, end+1) = (X1.^(i-j)).*(X2.^j);
     end
 end
-
 end
